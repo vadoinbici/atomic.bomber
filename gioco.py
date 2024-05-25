56c99da0097ac4ed5af324150aa699ff5350a4bd
vadoinbici <cesatiyuri@gmail.com>
2024-05-26 00:31:10 +0200

    controllo movimento aereo

--- gioco.py
+++ gioco.py
@@ -8,19 +8,35 @@ class Aereo:
         self.image = pygame.transform.scale(self.image, (100,50))
         self.rect = self.image.get_rect()      
         self.rect.topleft = (pos_x, pos_y)
-        self.vel_x = 4
-        self.vel_y = 2
+        # self.vel_x = 4
+        # self.vel_y = 2
     
     def cordinate(self):
         return self.rect.topleft
     
-    def muoviti (self):
+    def muoviti (self, vel_x, vel_y):
+        self.vel_x = vel_x
+        self.vel_y = vel_y
         self.rect.x += self.vel_x
         self.rect.y += self.vel_y
 
     def stampa (self, screen):
         screen.blit(self.image, self.rect.topleft)
     
+    def controllox (self):
+        if self.rect.x > 1200:
+            self.rect.x = -50
+        
+        if self.rect.x < -50:
+            self.rect.x = 1200
+            
+    def controlloy (self, vel_y):
+        if self.rect.y < -20:
+            return (-vel_y)
+        else:
+            return (vel_y)
+            
+
 aereo = Aereo (4,2,200,200)
 
 screen = pygame.display.set_mode((1200,600))
@@ -34,18 +50,32 @@ prova2 = pygame.image.load('immagini/aeroplano.png')
 prova = pygame.transform.scale(prova2, (100, 50))
 prova1 = prova.get_rect()
 
+vel_x = 4
+vel_y = 0
+
 while True:
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
            pygame.quit()
            exit()
+       
        elif event.type == pygame.KEYDOWN:
-           if event.key == pygame.K_LEFT:
-               
-    aereo.muoviti() 
+            if event.key == pygame.K_LEFT:
+               vel_x = -4
+            elif event.key == pygame.K_RIGHT:
+               vel_x = 4  
+            elif event.key == pygame.K_DOWN:
+               vel_y = 2 
+            elif event.key == pygame.K_UP:
+               vel_y = -2
+
+    
+    aereo.controllox()
+    vel_y = aereo.controlloy(vel_y)
+    aereo.muoviti(vel_x, vel_y) 
     screen.blit(sfondo, (0,0))
     aereo.stampa(screen)
-
+    
 
 
     pygame.display.update()
